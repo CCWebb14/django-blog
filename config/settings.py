1a7f792c1cc34f9a90e5eb8f47b4c0f1eb6e2105
CCWebb14 <CCWebb14@gmail.com>
2022-04-04 13:28:07 -0700

    reverted to insecure secret key for testing purposes

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-yu^6im2emho$cwxlzjiv0c(_$hq=**exbgiaog-=pv0x6j#)m)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
