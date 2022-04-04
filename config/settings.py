8452efddaba39238ea817e526769c1cd76155c51
Cooper Webb <cooperwebb@Coopers-MBP.hitronhub.home>
2022-04-04 12:04:14 -0700

    added login page and functionality, login/logout redirects in settings

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,7 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login redirect 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
